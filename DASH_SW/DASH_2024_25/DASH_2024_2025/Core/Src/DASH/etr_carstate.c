7560e0524541d9fda8cf75b2a9168d6bbcb68379
Max_laptop <maxespin28@gmail.com>
2025-07-06 15:00:39 +0200

    v1.0 working

--- DASH_SW/DASH_2024_25/DASH_2024_2025/Core/Src/DASH/etr_carstate.c
+++ DASH_SW/DASH_2024_25/DASH_2024_2025/Core/Src/DASH/etr_carstate.c
@@ -573,7 +573,8 @@ void drawScreen(void) {
             break;
 
         case DASH_5_INVERTERS:
-        			carState_14 ();
+        			if(printStatus==0)carState_14 ();
+        			printStatus++;
             break;
 
         case DASH_6_RACING_MODE:
@@ -587,7 +588,8 @@ void drawScreen(void) {
             break;
 
         case DASH_7_ERROR:
-        			carState_21 ();
+        			if(printStatus==0)carState_21 ();
+        			printStatus++;
             break;
 
         default:
